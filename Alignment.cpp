d73117308474cff9b589107fb4a3a2c2e5c60157
Connor Clark <connor.r.clark@gmail.com>
2014-05-27 10:58:33 -0600

    added mutation operator

--- Alignment.cpp
+++ Alignment.cpp
@@ -2,15 +2,16 @@
 
 #include <vector>
 #include <algorithm>
+#include <random>
 using namespace std;
 
-Alignment::Alignment(Network* net1, Network* net2, bool random = true){
+Alignment::Alignment(Network* net1, Network* net2, bool random){
 	unsigned int size = max(net1->nodeToNodeName.size(),
 		                    net2->nodeToNodeName.size());
 
 	aln = vector<node>(size,-1);
 
-	for(int i = 0; i < size i++){
+	for(int i = 0; i < size; i++){
 		aln[i] = i;
 	}
 
@@ -19,5 +20,22 @@ Alignment::Alignment(Network* net1, Network* net2, bool random = true){
 	}
 	else{
 		//todo: add seeding. For now just arbitrary alignment.
+	}
+}
+
+void Alignment::mutate(mt19937& prng, float mutswappb){
+	int size = aln.size();
+	uniform_real_distribution<float> fltgen(0.0,1.0);
+	uniform_int_distribution<int> intgen(0,size-2); 
+	for(int i = 0; i < aln.size(); i++){
+		if(fltgen(prng) < mutswappb){
+			int swapIndex = intgen(prng);
+			if(swapIndex >= i){
+				swapIndex++;
+			}
+			int temp = aln[swapIndex];
+			aln[swapIndex] = aln[i];
+			aln[i] = temp;
+		}
 	}
 }
--- Alignment.h
+++ Alignment.h
@@ -2,12 +2,12 @@
 
 #include <vector>
 #include <algorithm>
-
+#include <random>
 #include "Network.h"
 
 class Alignment{
 public:	
 	Alignment(Network* net1, Network* net2, bool random = true);
-
+	void mutate(mt19937& prng, float mutswappb);
 	vector<node> aln;
 };
