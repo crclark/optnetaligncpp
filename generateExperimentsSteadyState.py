b0da42849afa9a5c9ff4bb20917d6940547f158f
Connor Clark <connor.r.clark@gmail.com>
2014-07-01 11:38:50 -0600

    python script to generate experiments to find good parameter settings

---
+++ generateExperimentsSteadyState.py
@@ -0,0 +1,10 @@
+import random
+
+for i in xrange(500):
+	mutswappb = random.uniform(0.0001,0.001)
+	mutrate = random.uniform(0.01, 0.5)
+	cxswappb = random.uniform(0.1,1.0)
+	cxrate = random.uniform(0.1,0.9)
+	hillclimbiters = random.uniform(100, 10000)
+	dynparams = random.sample([True,False],1)
+	print "./steadystateMOGA --net1 ~/Dropbox/optnetalign/optnetalign/tests/dmela.net --net2 ~/Dropbox/optnetalign/optnetalign/tests/hsapi.net --annotations1 ~/Dropbox/optnetalign/optnetalign/tests/dmela.annos --annotations2 ~/Dropbox/optnetalign/optnetalign/tests/hsapi.annos --s3 --mutswappb " + str(mutswappb) + " --mutrate " + str(mutrate) + " --cxswappb " + str(cxswappb) + " --cxrate " + str(cxrate) + " --popsize 500 " + (" --dynparams " if dynparams else " ")  + "--nthreads 16 --nooutput --generations 1000000 --timelimit 60 --finalstats >> experimentalData.csv"
