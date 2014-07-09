92dec2caf3076ea5080a177c45f45c94cf8b2455
Connor Clark <connor.r.clark@gmail.com>
2014-07-09 14:31:42 -0600

    changed localTest back into a straightforward hillclimber. No special features.

--- localTest.cpp
+++ localTest.cpp
@@ -63,77 +63,15 @@ int main(int ac, char* av[])
 		//input time proportion.
 		//fast hill climb version
 		cout<<"starting main loop"<<endl;
-		VelocityTracker velTracker;
-		int gensWithoutImprovement = 0;
-		int mutsDone = 0;
-		vector<double> initSpeed;
-		vector<double> bestFits(fitnessNames.size(),0.0);
 		for(int i = 0; i < generations; i++){
-			auto oldFit = aln->fitness;
 			correctHillClimb(g, aln, total,
                500, fitnessNames);	
 			
-			//potentialBasedSearch(g, aln, total, 500, fitnessNames, false);
-			auto newFit = aln->fitness;
-			for(int q = 0; q < newFit.size(); q++){
-				if(newFit[q] > bestFits[q]){
-					bestFits[q] = newFit[q];
-				}
-			}
-			
-			vector<double> deltaFit;
-			for(int j = 0; j < newFit.size(); j++){
-				deltaFit.push_back(newFit[j] - oldFit[j]);
-			}
-			velTracker.reportDelta(deltaFit);
-
-			if(!dominates(newFit,oldFit)){
-				gensWithoutImprovement++;
-			}
-			else{
-				gensWithoutImprovement = 0;
-			}
-			if(false/*i > 10000 && gensWithoutImprovement == (i/100)*/){
-				aln->mutate(g, 0.0001, total);
-				aln->computeFitness(fitnessNames);
-				gensWithoutImprovement = 0;
-				mutsDone++;
-			}
 			for(int j = 0; j <fitnessNames.size();j++){
 				cout<<"current "<<fitnessNames.at(j)<<" is "
 				    <<aln->fitness.at(j)<<endl;
-				cout<<"best "<<fitnessNames.at(j)<<" is "
-				    <<bestFits.at(j)<<endl;
 			}
             cout<<"EC is "<<((double)(aln->currConservedCount))/((double)(net1->edges.size()))<<endl;
-            cout<<"Velocity is ";
-            vector<double> currVel = velTracker.getRecentVel();
-            for(int j = 0; j < currVel.size(); j++){
-            	cout<<currVel[j]<<' ';
-            }
-            cout<<endl;
-            cout<<"Velocity-based optimum detected: ";
-            bool belowThresh = true;
-            if(i > 50){			
-				for(int j = 0; j < currVel.size(); j++){
-					belowThresh &= currVel[j] 
-	                                < 0.001*initSpeed[j];
-				}
-			}
-			else{
-				belowThresh = false;
-			}
-
-			if(i == 50){
-				initSpeed = velTracker.getRecentVel();
-			}
-			if(belowThresh){
-				cout<<"yes."<<endl;
-			}
-			else{
-				cout<<"no."<<endl;
-			}
-            cout<<"Mutations performed: "<<mutsDone<<endl;
 			cout<<"Generation "<<i<<" complete."<<endl;
 		}
 		aln->save(outprefix + "_localTest.aln");
